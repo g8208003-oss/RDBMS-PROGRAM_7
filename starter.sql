8cc2d84e23f8786ca7a0249f45e326467681469c
1U25CS032- Gunasekaran T <g8208003@gmail.com>
2026-09-02 20:02:15 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,23 +1,9 @@
--- Marksheet Assignment
--- Student Name: __________________
--- Roll No: _______________________
-
--- STEP 1: Create the Marksheet table
--- Fields: RollNo, Name, Department, Marks
-
 CREATE TABLE Marksheet (
     RollNo INT PRIMARY KEY,
     Name VARCHAR(50),
     Department VARCHAR(20),
     Marks INT
 );
-
--- STEP 2: Insert the following records
--- 1 Arun CSE 85
--- 2 Divya IT 78
--- 3 Karthik CSE 92
--- 4 Nisha ECE 67
--- 5 Rahul IT 88
 
 INSERT INTO Marksheet (RollNo, Name, Department, Marks)
 VALUES
@@ -27,9 +13,7 @@ VALUES
 (4, 'Nisha', 'ECE', 67),
 (5, 'Rahul', 'IT', 88);
 
--- STEP 3:
--- Display students whose marks are greater than 80.
--- Sort the result in descending order of Marks.
-
--- Write your SELECT query below:
-
+SELECT RollNo, Name, Department, Marks
+FROM Marksheet
+WHERE Marks > 80
+ORDER BY Marks DESC;
